89b87899301ac723d2743a6461f3d93ac8232196
AV Shayan <shayan2004786@gmail.com>
2025-09-11 18:25:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const path = require('path');
-const logger = require('./middleware/logEvents')
-const mongoose = require('mongoose');
-const connectDB = require('./dbConn');
-const cors = require('cors');
+import ('dotenv') from 'dotenv;
+dotenv.config();
+import express from 'express';
+import app from express();
+import path from 'path';
+import logger from './middleware/logEvents';
+import mongoose from 'mongoose';
+import connectDB = from './dbConn';
+import cors from 'cors';
 const PORT = process.env.PORT || 3500;
 
 connectDB();
@@ -24,4 +25,5 @@ app.use('/tasks',require('./routes/API/tasks'));
 mongoose.connection.once('open',() => {    // Server will start only if MongoDB connected
     console.log("Connected To MongoDB");
     app.listen(PORT,() => console.log("Server running on PORT 3500"));
+
 });
